9fb1223d9a226ed2648dfef7a1423df9e563f33b
kusa <foxys130824@gmail.com>
2026-02-03 18:15:58 +0300

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-violet-700 h-20">
+      <p className="text-center text-4xl text-white font-bold pt-3 ">
+        Expense Tracker
+      </p>
+    </header>
+  );
+}
+
+export default Header;
